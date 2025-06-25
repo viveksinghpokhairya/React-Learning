19cc8e75397192524e3e1bc72936ab0f4cfcadab
vivek singh <pokhariyavivek01@gmail.com>
2025-06-25 15:33:17 +0530

    third commit

--- logic.js
+++ logic.js
@@ -3,6 +3,6 @@ const header_2 = React.createElement('h2', {id: "second", className: "class_"}, 
 const parent_div = React.createElement('div', {}, [header_1, header_2]);
 
 const root_1 = ReactDOM.createRoot(document.getElementById("root"));
-// root_1.render(parent_div);
-root_1.render(header_1);
-root_1.render(header_2);
+root_1.render(parent_div);
+// root_1.render(header_1);
+// root_1.render(header_2);
