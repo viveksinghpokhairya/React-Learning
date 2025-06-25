554120b9654abc404a334e64969cef92942ced6f
vivek singh <pokhariyavivek01@gmail.com>
2025-06-25 15:23:53 +0530

    my first commit

---
+++ logic.js
@@ -0,0 +1,8 @@
+const header_1 = React.createElement('h1', {id: "first", className: "class_", style:{backgroundColor: "grey", color: "white"}}, "Vivek Singh Pokhariya");
+const header_2 = React.createElement('h2', {id: "second", className: "class_"}, "Hello Coder Army");
+const parent_div = React.createElement('div', {}, [header_1, header_2]);
+
+const root_1 = ReactDOM.createRoot(document.getElementById("root"));
+// root_1.render(parent_div);
+root_1.render(header_1);
+root_1.render(header_2);
